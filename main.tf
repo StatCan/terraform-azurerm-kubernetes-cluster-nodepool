904c608744f45663c0002d165ceed9a45469532d
Michael Patsula <michael.patsula@cloud.statcan.ca>
2023-01-17 14:36:09 +0000

    Refactor: variable structure

--- main.tf
+++ main.tf
@@ -2,40 +2,39 @@
 #
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/kubernetes_cluster_node_pool
 #
-resource "azurerm_kubernetes_cluster_node_pool" "nodepool" {
+resource "azurerm_kubernetes_cluster_node_pool" "this" {
   name                  = var.name
   kubernetes_cluster_id = var.kubernetes_cluster_id
-
-  node_count           = !var.enable_auto_scaling ? var.node_count : null
-  orchestrator_version = var.kubernetes_version
-  zones                = var.zones
-
-  # Node configuration
-  vm_size               = var.vm_size
-  node_labels           = var.labels
-  node_taints           = var.taints
-  enable_node_public_ip = false
-  max_pods              = var.max_pods
-
-  # Disk configuration
-  enable_host_encryption = var.enable_host_encryption
-  os_disk_size_gb        = var.disk_size_gb
-  os_disk_type           = var.disk_type
-  os_type                = var.os_type
-  pod_subnet_id          = var.pod_subnet_id
+  orchestrator_version  = var.kubernetes_version
+  zones                 = var.availability_zones
 
   # Network configuration
-  vnet_subnet_id = var.vnet_subnet_id
+  vnet_subnet_id        = var.vnet_subnet_id
+  pod_subnet_id         = var.pod_subnet_id
+  enable_node_public_ip = false
 
-  # Upgrade configuration
-  upgrade_settings {
-    max_surge = var.uprade_max_surge
-  }
+  # Node configuration
+  vm_size     = var.vm_size
+  os_type     = var.os_type
+  node_labels = var.node_labels
+  node_taints = var.node_taints
+  max_pods    = var.max_pods
 
   # Autoscaling
+  node_count          = !var.enable_auto_scaling ? var.node_count : null
   enable_auto_scaling = var.enable_auto_scaling
   min_count           = var.enable_auto_scaling ? var.auto_scaling_min_nodes : null
   max_count           = var.enable_auto_scaling ? var.auto_scaling_max_nodes : null
+
+  # Disk configuration
+  enable_host_encryption = var.enable_host_encryption
+  os_disk_size_gb        = var.os_disk_size_gb
+  os_disk_type           = var.os_disk_type
+
+  # Upgrade configuration
+  upgrade_settings {
+    max_surge = var.upgrade_max_surge
+  }
 
   tags = var.tags
 }
--- variables.tf
+++ variables.tf
@@ -1,90 +1,145 @@
+######################
+### Azure Resource ###
+######################
+
 variable "name" {
-  description = "Name of the pool"
+  description = "The name of the node pool"
+  type        = string
 }
 
 variable "kubernetes_cluster_id" {
-  description = "ID of the Kubernetes cluster"
-}
-
-variable "node_count" {
-  type    = number
-  default = 3
-}
-
-variable "kubernetes_version" {
-  default = null
-}
-
-variable "zones" {
-  type    = list(string)
-  default = null
-}
-variable "vm_size" {
-  default = "Standard_D2s_v3"
-}
-
-variable "max_pods" {
-  default = 60
-}
-
-variable "labels" {
-  type    = map(string)
-  default = {}
-}
-
-variable "enable_host_encryption" {
-  type    = bool
-  default = false
-}
-
-variable "disk_size_gb" {
-  type    = number
-  default = 256
-}
-
-variable "disk_type" {
-  default = "Managed"
-}
-
-variable "os_type" {
-  default = "Linux"
-}
-
-variable "pod_subnet_id" {
-
-}
-
-variable "vnet_subnet_id" {
-
-}
-
-# Per documentation, https://docs.microsoft.com/en-us/azure/aks/upgrade-cluster#customize-node-surge-upgrade
-#   For production node pools, we recommend a max-surge setting of 33%.
-variable "uprade_max_surge" {
-  default = "33%"
-}
-
-variable "taints" {
-  type    = list(string)
-  default = []
+  description = "The ID of the Kubernetes cluster"
+  type        = string
 }
 
 variable "tags" {
-  type    = map(string)
-  default = {}
+  description = "A mapping of tags to assign to the resource."
+  type        = map(string)
+  default     = {}
+}
+
+##########################
+### Cluster Versioning ###
+##########################
+
+variable "kubernetes_version" {
+  description = "The version of Kubernetes used for the Agents."
+  type        = string
+  default     = null
+}
+
+################################
+### Networking Configuration ###
+################################
+
+variable "pod_subnet_id" {
+  description = "The ID of the Subnet where the pods in the Node Pool should exist."
+  type        = string
+}
+
+variable "vnet_subnet_id" {
+  description = "The ID of the Subnet where this Node Pool should exist."
+  type        = string
+}
+
+##########################
+### Node Configuration ###
+##########################
+
+variable "vm_size" {
+  description = "The SKU which should be used for the Virtual Machines used in this Node Pool."
+  type        = string
+  default     = "Standard_D2s_v3"
+}
+
+variable "os_type" {
+  description = "The Operating System which should be used for this Node Pool. Possible values are Linux and Windows."
+  type        = string
+  default     = "Linux"
+}
+
+variable "availability_zones" {
+  description = " Specifies a list of Availability Zones in which this Kubernetes Cluster Node Pool should be located."
+  type        = list(string)
+  default     = null
+}
+
+variable "max_pods" {
+  description = "The maximum number of pods that can run on each agent."
+  type        = number
+  default     = 60
+}
+
+variable "node_labels" {
+  description = "A map of Kubernetes labels which should be applied to nodes in this Node Pool."
+  type        = map(string)
+  default     = {}
+}
+
+variable "node_taints" {
+  description = "A list of Kubernetes taints which should be applied to nodes in the agent pool."
+  type        = list(string)
+  default     = []
+}
+
+##################
+### Node Count ###
+##################
+
+variable "node_count" {
+  description = "If enable_auto_scaling = false, it is the number of nodes which should exist within this Node Pool. Otherwise, it is the initial number of nodes to set."
+  type        = number
+  default     = null
 }
 
 variable "enable_auto_scaling" {
-  type    = bool
-  default = false
+  description = ""
+  type        = bool
+  default     = false
 }
 
 variable "auto_scaling_min_nodes" {
-  type    = number
-  default = 0
+  description = "The minimum number of nodes which should exist within this Node Pool."
+  type        = number
+  default     = 0
 }
 
 variable "auto_scaling_max_nodes" {
-  type    = number
-  default = 3
+  description = "The maximum number of nodes which should exist within this Node Pool."
+  type        = number
+  default     = 3
+}
+
+##########################
+### Disk Configuration ###
+##########################
+
+variable "enable_host_encryption" {
+  description = "Should the nodes in this Node Pool have host encryption enabled? Changing this forces a new resource to be created."
+  type        = bool
+  default     = false
+}
+
+variable "os_disk_size_gb" {
+  description = "The Agent Operating System disk size in GB. Changing this forces a new resource to be created."
+  type        = number
+  default     = 256
+}
+
+variable "os_disk_type" {
+  description = "The type of disk which should be used for the Operating System. Possible values are Ephemeral and Managed. Defaults to Managed. Changing this forces a new resource to be created."
+  type        = string
+  default     = "Managed"
+}
+
+#############################
+### Upgrade Configuration ###
+#############################
+
+# Per documentation, https://docs.microsoft.com/en-us/azure/aks/upgrade-cluster#customize-node-surge-upgrade
+#   For production node pools, we recommend a max-surge setting of 33%.
+variable "upgrade_max_surge" {
+  description = "The maximum number or percentage of nodes which will be added to the Node Pool size during an upgrade."
+  type        = string
+  default     = "33%"
 }
